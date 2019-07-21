60d1ef27cac47c8641ee7e6c4da3592a09ea2975
西野 <nisino333@gmail.com>
2019-07-21 13:54:05 +0900

    UI自動テスト追加

--- src/test/java/com/example/demo/monitor/monitorTest.java
+++ src/test/java/com/example/demo/monitor/monitorTest.java
@@ -1,26 +1,29 @@
-package com.example.demo.monitor;
-
-import org.junit.BeforeClass;
-import org.junit.Test;
-
-import com.codeborne.selenide.Configuration;
-//import static com.codeborne.selenide.Condition.*;//static importしても良い。
-import com.codeborne.selenide.Selenide;
-//import static com.codeborne.selenide.Selenide.*;//static importしても良い。
-import com.codeborne.selenide.WebDriverRunner;
-
-public class monitorTest {
-    @BeforeClass
-    public static void beforeClass() {
-        // デフォルトではFireFoxを使うので、Chromeを使うよう設定している。
-        Configuration.browser = WebDriverRunner.CHROME;
-
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
-    }
-
-	@Test
-	public void test1() {
-		// ページを開く
-		Selenide.open("https://www.yahoo.co.jp/");
-	}
-}
+//package com.example.demo.monitor;
+//
+//import org.junit.BeforeClass;
+//import org.junit.Test;
+//
+//import com.codeborne.selenide.Configuration;
+//import com.codeborne.selenide.Selenide;
+//import com.codeborne.selenide.WebDriverRunner;
+//
+//public class monitorTest {
+//  @BeforeClass
+//  public static void beforeClass() {
+//    // デフォルトではFireFoxを使うので、Chromeを使うよう設定している。
+//    Configuration.browser = WebDriverRunner.CHROME;
+//  }
+//
+//  @Test
+//  public void testSample() {
+//    // ページを開く
+//    Selenide.open("http://localhost:8080/");
+//    Selenide.$(".indexBtn").click();
+//    Selenide.$(".dateName").val("サンプル");
+//    Selenide.$(".dateStandart").val("20190101");
+//    Selenide.$(".calulationYear").val("1");
+//    Selenide.$(".calulationMonth").val("1");
+//    Selenide.$(".calulationDay").val("1");
+//    Selenide.$(".postBtn").click();
+//  }
+//}
